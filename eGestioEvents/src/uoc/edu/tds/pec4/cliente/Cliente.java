53944e45b0084354b22648c51313bd565605b17f
asolde@gmail.com <asolde@gmail.com@a12747cf-5093-dce4-9e78-ec0dbd0bd039>
2011-05-04 19:27:57 +0000

    estas clases hay que implementarlas cuando con un patrón

--- eGestioEvents/src/uoc/edu/tds/pec4/cliente/Cliente.java
+++
@@ -1,5 +0,0 @@
-package uoc.edu.tds.pec4.cliente;
-
-public class Cliente {
-
-}
--- eGestioEvents/src/uoc/edu/tds/pec4/servidor/Servidor.java
+++
@@ -1,7 +0,0 @@
-package uoc.edu.tds.pec4.servidor;
-
-import javax.swing.JFrame;
-
-public class Servidor extends JFrame {
-	
-}
