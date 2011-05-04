53fb19a6b904a61ccb22ba6c2e2eefd7b0a7107a
jgarciaegea@gmail.com <jgarciaegea@gmail.com@a12747cf-5093-dce4-9e78-ec0dbd0bd039>
2011-05-04 22:58:14 +0000

    getters & setters

--- eGestioEvents/src/uoc/edu/tds/pec4/beans/CentroDocente.java
+++ eGestioEvents/src/uoc/edu/tds/pec4/beans/CentroDocente.java
@@ -52,4 +52,116 @@ public class CentroDocente implements BeanInterface, Serializable{
 	public CentroDocente() {
 		super();
 	}
+
+
+	/**
+	 * @return the idCentro
+	 */
+	public Integer getIdCentro() {
+		return idCentro;
+	}
+
+
+	/**
+	 * @param idCentro the idCentro to set
+	 */
+	public void setIdCentro(Integer idCentro) {
+		this.idCentro = idCentro;
+	}
+
+
+	/**
+	 * @return the estado
+	 */
+	public Integer getEstado() {
+		return estado;
+	}
+
+
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(Integer estado) {
+		this.estado = estado;
+	}
+
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	/**
+	 * @return the motivoEstado
+	 */
+	public String getMotivoEstado() {
+		return motivoEstado;
+	}
+
+
+	/**
+	 * @param motivoEstado the motivoEstado to set
+	 */
+	public void setMotivoEstado(String motivoEstado) {
+		this.motivoEstado = motivoEstado;
+	}
+
+
+	/**
+	 * @return the universidad
+	 */
+	public Universidad getUniversidad() {
+		return universidad;
+	}
+
+
+	/**
+	 * @param universidad the universidad to set
+	 */
+	public void setUniversidad(Universidad universidad) {
+		this.universidad = universidad;
+	}
+
+
+	/**
+	 * @return the fechaAlta
+	 */
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+
+
+	/**
+	 * @param fechaAlta the fechaAlta to set
+	 */
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+
+	/**
+	 * @return the fechaEstado
+	 */
+	public Date getFechaEstado() {
+		return fechaEstado;
+	}
+
+
+	/**
+	 * @param fechaEstado the fechaEstado to set
+	 */
+	public void setFechaEstado(Date fechaEstado) {
+		this.fechaEstado = fechaEstado;
+	}
 }
